c874aa6c3233bd7204ce489e1a01dee82d83fb20
Csaba Gyuris <gyuris.csaba@vasvari.org>
2024-01-29 16:36:20 +0100

    KretaContext osztály options elfogadó konstruktora

--- Kreta.Backend/Context/KretaContext.cs
+++ Kreta.Backend/Context/KretaContext.cs
@@ -4,5 +4,8 @@ namespace Kreta.Backend.Context
 {
     public class KretaContext : DbContext
     {
+        public KretaContext(DbContextOptions options) : base(options)
+        {
+        }
     }
 }
