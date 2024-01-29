082f5539f6a4cf8de9beffa810de71994575d29c
Csaba Gyuris <gyuris.csaba@vasvari.org>
2024-01-29 16:49:18 +0100

    Tesztadatok

--- Kreta.Backend/Context/ModelBuilderExtension.cs
+++ Kreta.Backend/Context/ModelBuilderExtension.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Kreta.Backend.Datas.Entities;
+using Kreta.Backend.Datas.Enums;
+using Microsoft.EntityFrameworkCore;
 
 namespace Kreta.Backend.Context
 {
@@ -6,6 +8,32 @@ namespace Kreta.Backend.Context
     {
         public static void Seed(this ModelBuilder modelBuilder)
         {
+            List<Student> students = new List<Student>
+            {
+                new Student
+                {
+                    Id=Guid.NewGuid(),
+                    FirstName="János",
+                    LastName="Jegy",
+                    BirthsDay=new DateTime(2022,10,10),
+                    SchoolYear=9,
+                    SchoolClass = SchoolClassType.ClassA,
+                    EducationLevel="érettségi"
+                },
+                new Student
+                {
+                    Id=Guid.NewGuid(),
+                    FirstName="Szonja",
+                    LastName="Stréber",
+                    BirthsDay=new DateTime(2021,4,4),
+                    SchoolYear=10,
+                    SchoolClass = SchoolClassType.ClassB,
+                    EducationLevel="érettségi"
+                }
+            };
+
+            // Students
+            modelBuilder.Entity<Student>().HasData(students);
         }
     }
 }
