d9ad8a53a07e788ab77acf85b798cdd20ff5f645
Csaba Gyuris <gyuris.csaba@vasvari.org>
2024-01-29 17:25:05 +0100

    Dependency injection (függőség beinjektálása)

--- Kreta.Backend/Repos/StudentRepo.cs
+++ Kreta.Backend/Repos/StudentRepo.cs
@@ -1,6 +1,23 @@
-﻿namespace Kreta.Backend.Repos
+﻿using Kreta.Backend.Context;
+using Kreta.Backend.Datas.Entities;
+
+namespace Kreta.Backend.Repos
 {
     public class StudentRepo : IStudentRepo
     {
+        private readonly KretaInMemoryContext _dbContext;
+        public StudentRepo(KretaInMemoryContext dbContext)
+        {            
+            _dbContext = dbContext;
+        }
+        public Task<Student> GetByIdAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Student>> SelectStudentAsync()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
