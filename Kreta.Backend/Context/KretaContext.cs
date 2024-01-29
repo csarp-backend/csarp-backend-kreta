12704e232ae1fd64efddc61280436970eaf01b93
Csaba Gyuris <gyuris.csaba@vasvari.org>
2024-01-29 16:53:22 +0100

    DbSet-et kersztül Students néven elérhetővé válik az adatázis Student táblája

--- Kreta.Backend/Context/KretaContext.cs
+++ Kreta.Backend/Context/KretaContext.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Kreta.Backend.Datas.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace Kreta.Backend.Context
 {
     public class KretaContext : DbContext
     {
+        public DbSet<Student> Students { get; set; }
         public KretaContext(DbContextOptions options) : base(options)
         {
         }
