750faeb92fd268d91fb672732800902e2c35fa25
Csaba Gyuris <gyuris.csaba@vasvari.org>
2024-01-29 17:36:03 +0100

    StudentController osztály

---
+++ Kreta.Backend/Controllers/StudentController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Kreta.Backend.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class StudentController : ControllerBase
+    {
+    }
+}
