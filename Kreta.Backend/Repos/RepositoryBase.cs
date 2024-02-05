035e584af49453b62aa2b4ae830566869d5a16a8
Csaba Gyuris <gyuris.csaba@vasvari.org>
2024-02-05 17:00:28 +0100

    RepositoryBase generikus osztály amely egy DbContext-től és egy osztálytól (pl. Student) függ

---
+++ Kreta.Backend/Repos/RepositoryBase.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Kreta.Backend.Repos
+{
+    public class RepositoryBase<TDbContext, TEntity> : IRepositoryBase<TEntity>
+        where TDbContext: DbContext
+        where TEntity : class
+    {
+
+    }
+}
