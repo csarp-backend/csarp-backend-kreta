2b97f7fa68d8301ea5ef962bddc00511debf0907
Csaba Gyuris <gyuris.csaba@vasvari.org>
2024-02-05 17:58:30 +0100

    StudentRepo osztály fejléce

---
+++ Kreta.Backend/Repos/StudentRepo.cs
@@ -0,0 +1,8 @@
+﻿using Kreta.Backend.Datas.Entities;
+
+namespace Kreta.Backend.Repos
+{
+    public class StudentRepo<TDbContext> : RepositoryBase<TDbContext,Student>, IStudentRepo
+    {
+    }
+}
