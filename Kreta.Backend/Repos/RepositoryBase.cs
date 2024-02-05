289d9de93b4b6e2415ff6a7d34800d42f19c31ca
Csaba Gyuris <gyuris.csaba@vasvari.org>
2024-02-05 17:08:50 +0100

    Impltementáljuk az interface-t

--- Kreta.Backend/Repos/RepositoryBase.cs
+++ Kreta.Backend/Repos/RepositoryBase.cs
@@ -1,12 +1,36 @@
 ﻿using Kreta.Shared.Models;
 using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
 
 namespace Kreta.Backend.Repos
 {
     public abstract class RepositoryBase<TDbContext, TEntity> : IRepositoryBase<TEntity>
-        where TDbContext: DbContext
+        where TDbContext : DbContext
         where TEntity : class, IDbEntity<TEntity>, new()
     {
+        public void Create(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
 
+        public void Delete(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<TEntity> FindAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<TEntity> FindByCondition(Expression<Func<TEntity, bool>> expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
