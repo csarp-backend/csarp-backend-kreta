2370d3adf8258b89293e73121101f5f3aa629f3f
Csaba Gyuris <gyuris.csaba@vasvari.org>
2024-02-05 17:59:25 +0100

    A repo valamilyen DbContext típusú adatbázissal műküdik

--- Kreta.Backend/Repos/StudentRepo.cs
+++ Kreta.Backend/Repos/StudentRepo.cs
@@ -1,8 +1,10 @@
 ﻿using Kreta.Backend.Datas.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace Kreta.Backend.Repos
 {
     public class StudentRepo<TDbContext> : RepositoryBase<TDbContext,Student>, IStudentRepo
+        where TDbContext : DbContext
     {
     }
 }
